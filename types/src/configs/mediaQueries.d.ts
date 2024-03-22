62c5b60a9e0a093d3c4b57497e7ba296aa6319c6
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2024-03-22 17:35:59 +0100

    Update dist

---
+++ types/src/configs/mediaQueries.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Breakpoints a.k.a. Mediaqueries
+ */
+declare const _default: {
+    name: string;
+    query: string;
+    min: number;
+    max: number;
+    colCount: number;
+}[];
+export default _default;
