5a908f3ae125d979cdf0cb951776a9c13b767855
izabela.horodenska <izabla.horodenska@diva-e.com>
2024-03-27 16:09:44 +0100

    generatedStars funtion created

--- src/blocks/recommendation-bar/recommendation-bar.ts
+++ src/blocks/recommendation-bar/recommendation-bar.ts
@@ -6,9 +6,19 @@ interface RecommendationBarTemplateArgs {
   headline?: string;
   text?: string;
   url?: string;
+  rating: number;
 }
 
 const recommendationBarTemplate = (args: RecommendationBarTemplateArgs) => {
+  const generateStars = (rating: number) => {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+      stars.push(
+        html`<dva-e-icon icon-id="dva-icon-star" class="dva-e-rating-star dva-e-rating-star--full"></dva-e-icon>`,
+      );
+    }
+    return stars;
+  };
   return html`
     <div class="dvag-m-recommendation-bar">
       <div class="dvag-grid">
@@ -19,20 +29,10 @@ const recommendationBarTemplate = (args: RecommendationBarTemplateArgs) => {
           </div>
           <div class="dvag-m-recommendation-bar__cta-wrapper dvag-grid-col-1-12 dvag-grid-col-3-5 dvag-grid-col-4-4">
             <div class="dvag-m-recommendation-bar__rating-wrapper">
-              <dva-m-rating-box class="dva-m-rating-box" rating="5" edit-mode="false">
-                <div class="dva-m-rating-box__ratingstar-wrapper">
-                  <dva-e-icon icon-id="dva-icon-star" class="dva-e-rating-star dva-e-rating-star--full"></dva-e-icon>
-
-                  <dva-e-icon icon-id="dva-icon-star" class="dva-e-rating-star dva-e-rating-star--full"></dva-e-icon>
-
-                  <dva-e-icon icon-id="dva-icon-star" class="dva-e-rating-star dva-e-rating-star--full"></dva-e-icon>
-
-                  <dva-e-icon icon-id="dva-icon-star" class="dva-e-rating-star dva-e-rating-star--full"></dva-e-icon>
-
-                  <dva-e-icon icon-id="dva-icon-star" class="dva-e-rating-star dva-e-rating-star--full"></dva-e-icon>
-                </div>
+              <dva-m-rating-box class="dva-m-rating-box" rating="${args.rating}" edit-mode="false">
+                <div class="dva-m-rating-box__ratingstar-wrapper">${generateStars(args.rating)}</div>
               </dva-m-rating-box>
-              <p class="dvag-m-recommendation-bar__rating-label">5 von 5 Sternen</p>
+              <p class="dvag-m-recommendation-bar__rating-label">${args.rating} von 5 Sternen</p>
             </div>
             <dvag-e-overlay-toggle
               class="dvag-e-overlay-toggle dva-e-button dva-e-button--rebrush dva-e-button--small dva-e-button--white"
@@ -53,7 +53,8 @@ export default function (block: HTMLElement) {
   const headline = block.children[0].textContent || "";
   const text = block.children[1].textContent || "";
   const url = block.children[2].textContent || "";
+  const rating = block.children[3].textContent || "";
   cleanUpBlock(block);
 
-  render(recommendationBarTemplate({ headline, text, url }), block);
+  render(recommendationBarTemplate({ headline, text, url, rating }), block);
 }
