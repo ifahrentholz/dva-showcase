8e59e046e206e17815119494e7839c4853b45e51
Samuele Calamita <samuele.calamita@diva-e.com>
2024-03-27 10:38:17 +0100

    run build

---
+++ dist/__chunks__/dva-e-icon.template.tppk8KWP.js
@@ -0,0 +1,2 @@
+import{x as r}from"./lit-element.XkUWx5ik.js";const e=(n,o)=>r`<dva-e-icon class="dva-e-icon${o?` ${o}`:""}" icon-id="${n}"></dva-e-icon>`;export{e as r};
+//# sourceMappingURL=dva-e-icon.template.tppk8KWP.js.map
