9f2cfb4ee86797fc68267bb7d2d0b2d608e82af1
izabela.horodenska <izabla.horodenska@diva-e.com>
2024-03-27 11:48:10 +0100

    created quicklinks block

---
+++ src/blocks/quicklinks/quicklinks.ts
@@ -0,0 +1,56 @@
+import { cleanUpBlock } from "Utils/cleanUpBlock";
+import { html, render } from "lit";
+import "./quicklink.scss";
+
+interface QuicklinkTemplateArgs {
+  url?: string | null;
+  name?: string | null;
+  src: string | null;
+}
+
+const quicklinkTemplateOverview = (args: QuicklinkTemplateArgs[]) => {
+  return html`
+    <div class="dvag-m-c24-quicklinks dvag-m-skew-component" id="quicklinks">
+      <div class="dva-h-skew--in"></div>
+      <div class="dvag-m-skewed-box__content">
+        <div class="dvag-m-section-header"></div>
+
+        <div class="dvag-m-c24-quicklinks__content-wrapper wcm-io-parsys">
+          <dvag-m-c24-quicklink-item
+            class="dvag-m-c24-quicklink-item dvag-m-c24-quicklink-item--overlay-opener dva-state-active"
+            no-cookie-hide="no-cookie-hide"
+          >
+            ${args.map(
+              item => html`
+                <a class="dvag-m-c24-quicklink-item__link" target="_self" href="${item.url}">
+                  <dva-e-lazy-image
+                    class="dva-e-lazy-image dva-js-lazy-image dvag-m-c24-quicklink-item__icon dva-state-initialized dva-state-invp dva-state-loaded"
+                    src="${item.src}"
+                    alt="Icon"
+                    aspect-ratio="1:1"
+                  ></dva-e-lazy-image>
+                  <p class="dvag-m-c24-quicklink-item__label">${item.name}</p>
+                </a>
+              `,
+            )}
+          </dvag-m-c24-quicklink-item>
+        </div>
+      </div>
+      <div class="dva-h-skew--out"></div>
+    </div>
+  `;
+};
+
+export default function (block: HTMLElement) {
+  const rows = [...block.children];
+  const templateArgs = rows.map(row => {
+    const name = row.children[1].textContent;
+    const url = row.children[2].textContent;
+    const icon: HTMLImageElement | null = row.children[0].querySelector("picture > img");
+    const src = icon?.src;
+    return { name, url, src };
+  });
+
+  cleanUpBlock(block);
+  render(quicklinkTemplateOverview(templateArgs), block);
+}
