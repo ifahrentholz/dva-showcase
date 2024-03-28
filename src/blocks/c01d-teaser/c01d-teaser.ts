369f5599154697c3325bd3a4fdb5f818084127fa
izabela.horodenska <izabla.horodenska@diva-e.com>
2024-03-28 16:02:00 +0100

    code optimization

--- src/blocks/c01d-teaser/c01d-teaser.ts
+++ src/blocks/c01d-teaser/c01d-teaser.ts
@@ -5,16 +5,14 @@ import "./c01d-teaser.scss";
 interface TeaserTemplateArgs {
   subheadline?: string | null;
   headline?: string | null;
-  text?: string | null;
+  textFirstPart?: string | null;
+  textSecondPart?: string | null;
 }
 
 const teaserTemplateOverview = (args: TeaserTemplateArgs) => {
   return html`
     <div
-      class="c01d-teaser teaser dvag-h-background--gradient
-dvag-h-margin-bottom--none
-dvag-h-text--right
-dvag-h-headline--bold"
+      class="c01d-teaser teaser dvag-h-background--gradient dvag-h-margin-bottom--none dvag-h-text--right dvag-h-headline--bold"
     >
       <div class="dvag-m-c01d-teaser  " style="--gradient-color:#CCDDE4">
         <div class="dvag-m-c01d-teaser__image-wrapper">
@@ -27,7 +25,6 @@ dvag-h-headline--bold"
             aspect-ratio="21:9"
           >
           </dva-e-lazy-image>
-
           <dva-e-lazy-image
             class="dva-e-lazy-image dva-js-lazy-image dva-h-preload dvag-m-c01d-teaser__image dva-h-preload--21-9 dvag-m-c01d-teaser__image--desktop dva-state-initialized dva-state-invp dva-state-loaded"
             src="/content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/original./teaser-ki-klopp-21_9.jpg"
@@ -43,15 +40,10 @@ dvag-h-headline--bold"
             <h4 class="dvag-m-c01d-teaser__subheadline">${args.subheadline}</h4>
             <div class="dvag-m-c01d-teaser__headline">${args.headline}</div>
             <div class="dvag-m-c01d-teaser__text">
-              <p>${args.text}</p>
-              <p>
-                Wie auch immer Sie Ihr Leben leben wollen: Ich helfe Ihnen, die richtigen finanziellen Entscheidungen
-                zu&nbsp;treffen.
-              </p>
+              <p>${args.textFirstPart}</p>
+              <p>${args.textSecondPart}</p>
             </div>
             <div class="dvag-m-c01d-teaser__button-wrapper">
-              <!-- linkType: vbcontactoverlay -->
-
               <dvag-e-c26-vb-contact-overlay-toggle
                 id="contact_genericLink"
                 class="dvag-e-c26-vb-contact-overlay-toggle dva-e-button dva-e-button--rebrush dva-e-button--small dva-state-active"
@@ -73,8 +65,9 @@ dvag-h-headline--bold"
 export default function (block: HTMLElement) {
   const subheadline = block.children[0].textContent;
   const headline = block.children[1].textContent;
-  const text = block.children[2].textContent;
+  const textFirstPart = block.children[2].textContent;
+  const textSecondPart = block.children[3].textContent;
 
   cleanUpBlock(block);
-  render(teaserTemplateOverview({ subheadline, headline, text }), block);
+  render(teaserTemplateOverview({ subheadline, headline, textFirstPart, textSecondPart }), block);
 }
