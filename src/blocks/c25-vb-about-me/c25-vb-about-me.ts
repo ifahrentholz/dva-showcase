75260578a4ddb7ea47cb1d8ab74f7d07fa3ff37d
izabela.horodenska <izabla.horodenska@diva-e.com>
2024-04-01 15:17:12 +0200

    image, description optimization

--- src/blocks/c25-vb-about-me/c25-vb-about-me.ts
+++ src/blocks/c25-vb-about-me/c25-vb-about-me.ts
@@ -6,7 +6,8 @@ interface AboutMeTemplateArgs {
   name?: string | null;
   headline?: string | null;
   text?: string | null;
-  img?: string | null;
+  images?: string[] | null;
+  descriptions?: string[] | null;
 }
 
 const aboutMeTemplateOverview = (args: AboutMeTemplateArgs) => {
@@ -31,55 +32,34 @@ const aboutMeTemplateOverview = (args: AboutMeTemplateArgs) => {
                 </a>
               </div>
             </div>
-
             <div class="dvag-m-c25-vb-about-me__gallery-wrapper">
               <dvag-m-basic-slider
                 class="dvag-m-basic-slider dvag-m-c25-vb-about-me__slider"
                 active-on-mq="1-3"
                 title="Benjamin Rube - Lernen Sie mich kennen"
               >
-                <div class="dvag-m-basic-slider__slides dvag-m-c25-vb-about-me__slides">
-                  <div
-                    class="dvag-m-basic-slider__slide dvag-m-c25-vb-about-me__gallery-slide dvag-m-c25-vb-about-me__gallery-overlay-toggle dva-state-invp"
-                  >
-                    <dva-e-lazy-image
-                      class="dva-e-lazy-image dva-js-lazy-image dva-state-initialized dva-state-invp dva-state-loaded"
-                      src="/content/dam/vbdata/default-assets/images/editorConfigImageGallery-image-1.jpg/_jcr_content/renditions/original./editorConfigImageGallery-image-1.jpg"
-                      alt="Immer im Mittelpunkt der Beratung: Unsere Kunden."
-                      aspect-ratio="16:9"
-                    ></dva-e-lazy-image>
-                    <p class="dvag-m-basic-slider__slide-text" style="width: 535px;">
-                      Immer im Mittelpunkt der Beratung: Unsere Kunden.
-                    </p>
-                  </div>
-
-                  <div
-                    class="dvag-m-basic-slider__slide dvag-m-c25-vb-about-me__gallery-slide dvag-m-c25-vb-about-me__gallery-overlay-toggle dva-state-invp"
-                  >
-                    <dva-e-lazy-image
-                      class="dva-e-lazy-image dva-js-lazy-image dva-state-initialized dva-state-invp dva-state-loaded"
-                      src="/content/dam/vbdata/default-assets/images/editorConfigImageGallery-image-2.jpg/_jcr_content/renditions/original./editorConfigImageGallery-image-2.jpg"
-                      alt="Gemeinsam Finanzen gestalten."
-                      aspect-ratio="1:1"
-                    ></dva-e-lazy-image>
-                    <p class="dvag-m-basic-slider__slide-text" style="width: 194px;">Gemeinsam Finanzen gestalten.</p>
-                  </div>
-
-                  <div
-                    class="dvag-m-basic-slider__slide dvag-m-c25-vb-about-me__gallery-slide dvag-m-c25-vb-about-me__gallery-overlay-toggle dva-state-invp"
-                  >
-                    <dva-e-lazy-image
-                      class="dva-e-lazy-image dva-js-lazy-image dva-state-initialized dva-state-invp dva-state-loaded"
-                      src="/content/dam/vbdata/default-assets/images/editorConfigImageGallery-image-3.jpg/_jcr_content/renditions/original./editorConfigImageGallery-image-3.jpg"
-                      alt="Finanzcoaching ist Lebensplanung."
-                      aspect-ratio="1:1"
-                    ></dva-e-lazy-image>
-                    <p class="dvag-m-basic-slider__slide-text" style="width: 302px;">
-                      Finanzcoaching ist Lebensplanung.
-                    </p>
-                  </div>
-                </div>
-
+              <div class="dvag-m-basic-slider__slides dvag-m-c25-vb-about-me__slides">
+                ${
+                  args.images &&
+                  args.images.map(
+                    (img, index) => html`
+                      <div
+                        class="dvag-m-basic-slider__slide dvag-m-c25-vb-about-me__gallery-slide dvag-m-c25-vb-about-me__gallery-overlay-toggle dva-state-invp"
+                      >
+                        <dva-e-lazy-image
+                          class="dva-e-lazy-image dva-js-lazy-image dva-state-initialized dva-state-invp dva-state-loaded"
+                          src="${img}"
+                          alt="${args.descriptions ? args.descriptions[index] : ""}"
+                          aspect-ratio="${index === 0 ? "16:9" : "1:1"}"
+                        ></dva-e-lazy-image>
+                        <p class="dvag-m-basic-slider__slide-text" style="width: 535px;">
+                          ${args.descriptions ? args.descriptions[index] : ""}
+                        </p>
+                      </div>
+                    `,
+                  )
+                }
+              </div>
                 <div class="dvag-m-basic-slider__controls dvag-m-c25-vb-about-me__controls">
                   <button
                     class="dva-e-button dva-e-button--primary dva-e-button--rebrush dvag-m-basic-slider__nav-button dvag-m-basic-slider__nav-button--prev"
@@ -115,12 +95,30 @@ const aboutMeTemplateOverview = (args: AboutMeTemplateArgs) => {
 };
 
 export default function (block: HTMLElement) {
-  const name = block.children[0].textContent;
-  const headline = block.children[1].textContent;
-  const text = block.children[2].textContent;
-  const img = block.children[3].textContent;
-  console.log(img)
+  const name = block.children[0].textContent || "";
+  const headline = block.children[1].textContent || "";
+  const text = block.children[2].textContent || "";
+  const aboutMeMediaLinks: { img: string; description: string }[] = [];
+
+  for (let i = 3; i < block.children.length; i++) {
+    const imgElement = block.children[i].querySelector("div > picture > img");
+    const textElement = block.children[i].querySelector("div:nth-child(2)");
+    if (imgElement && textElement) {
+      const imgSrc = (imgElement as HTMLImageElement).getAttribute("src") || "";
+      const description = textElement.textContent || "";
+      aboutMeMediaLinks.push({ img: imgSrc, description });
+    }
+  }
 
   cleanUpBlock(block);
-  render(aboutMeTemplateOverview({ name, headline, text, img }), block);
+  render(
+    aboutMeTemplateOverview({
+      name,
+      headline,
+      text,
+      images: aboutMeMediaLinks.map(link => link.img),
+      descriptions: aboutMeMediaLinks.map(link => link.description),
+    }),
+    block,
+  );
 }
