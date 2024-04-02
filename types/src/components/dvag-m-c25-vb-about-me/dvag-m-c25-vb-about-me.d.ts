1085818c0096fe89c1597cb105897f125623e052
Samuele Calamita <samuele.calamita@diva-e.com>
2024-04-02 18:17:38 +0200

    refactor about me

--- types/src/components/dvag-m-c25-vb-about-me/dvag-m-c25-vb-about-me.d.ts
+++ types/src/components/dvag-m-c25-vb-about-me/dvag-m-c25-vb-about-me.d.ts
@@ -1,17 +1,17 @@
 import { Component } from "@kluntje/core";
-import { DvaLazyImage } from "Components/dva-e-lazy-image/dva-e-lazy-image";
 import { DvagBasicOverlay } from "Components/dvag-m-basic-overlay/dvag-m-basic-overlay";
+import { DVALazyImage } from "Components/dva-e-lazy-image/dva-e-lazy-image";
 export declare class DvagC25VbAboutMe extends Component {
     viewportObserver: IntersectionObserver;
     gallerySlides: Array<HTMLDivElement>;
-    galleryImages: Array<DvaLazyImage>;
+    galleryImages: Array<DVALazyImage>;
     galleryOverlayToggles: Array<HTMLDivElement>;
     galleryCounter: HTMLSpanElement;
     overlay: DvagBasicOverlay;
     afterComponentRender(): void;
     initGalleryButton(): void;
     handleLoadedImages({ detail }: any): void;
-    setCaptionWidth(image: DvaLazyImage): void;
+    setCaptionWidth(image: DVALazyImage): void;
     handleOverlayTogglesClick(): void;
     handleViewportChange(): void;
     handleMQChange(): void;
