4ec11ffbdff20f8bdd4bbb3ccee3f98cbdaffa87
izabela.horodenska <izabla.horodenska@diva-e.com>
2024-04-02 15:11:45 +0200

    teaser rebuild

--- src/blocks/c01d-teaser/c01d-teaser.ts
+++ src/blocks/c01d-teaser/c01d-teaser.ts
@@ -1,26 +1,32 @@
-import { cleanUpBlock } from "Utils/cleanUpBlock";
 import { html, render } from "lit";
 import "./c01d-teaser.scss";
+import { cleanUpBlock } from "Utils/cleanUpBlock";
 
-interface TeaserTemplateArgs {
-  subheadlineRight?: string | null;
+interface TeaserSharingTemplateArgs {
+  name?: string | null;
+  url?: string | null;
 }
 
-const teaserTemplateOverview = (args: TeaserTemplateArgs) => {
+const teaserTemplateOverview = (args: TeaserSharingTemplateArgs[]) => {
   return html`
-    <div class="c01d-teaser teaser dvag-h-background--gradient
-      dvag-h-margin-bottom--none
-      dvag-h-text--right
-      dvag-h-headline--bold">
-      <div class="dvag-m-c01d-teaser" style="--gradient-color:#CCDDE4">
+    <div
+      class="c01d-teaser teaser dvag-h-background--gradient
+dvag-h-margin-bottom--none
+dvag-h-text--right
+dvag-h-headline--bold"
+    >
+      <div class="dvag-m-c01d-teaser  " style="--gradient-color:#CCDDE4">
         <div class="dvag-m-c01d-teaser__image-wrapper">
+         
+
           <dva-e-lazy-image
             class="dva-e-lazy-image dva-js-lazy-image dva-h-preload dvag-m-c01d-teaser__image dva-h-preload--21-9 dvag-m-c01d-teaser__image--mobile"
             src="/content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original./teaser-ki-klopp-21_9-mobil.jpg"
             alt="teaser-ki-klopp-21_9-mobil.jpg"
             srcset="/content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.480.206.file/teaser-ki-klopp-21_9-mobil.jpg 480w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.760.326.file/teaser-ki-klopp-21_9-mobil.jpg 760w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.1024.439.file/teaser-ki-klopp-21_9-mobil.jpg 1024w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg./cq5dam.web.1280.1280.jpeg 1280w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.1440.617.file/teaser-ki-klopp-21_9-mobil.jpg 1440w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original./teaser-ki-klopp-21_9-mobil.jpg 1460w"
             sizes="(min-width: 1460px) 1460px, 100vw"
-            aspect-ratio="21:9">
+            aspect-ratio="21:9"
+          >
           </dva-e-lazy-image>
 
           <dva-e-lazy-image
@@ -29,12 +35,13 @@ const teaserTemplateOverview = (args: TeaserTemplateArgs) => {
             alt="teaser-ki-klopp-21_9.jpg"
             srcset="/content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/original.image_file.480.206.file/teaser-ki-klopp-21_9.jpg 480w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/original.image_file.760.326.file/teaser-ki-klopp-21_9.jpg 760w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/original.image_file.1024.439.file/teaser-ki-klopp-21_9.jpg 1024w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg./cq5dam.web.1280.1280.jpeg 1280w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/original.image_file.1440.617.file/teaser-ki-klopp-21_9.jpg 1440w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/original./teaser-ki-klopp-21_9.jpg 1460w"
             sizes="(min-width: 1460px) 1460px, 100vw"
-            aspect-ratio="21:9">
+            aspect-ratio="21:9"
+          >
           </dva-e-lazy-image>
         </div>
         <div class="dvag-m-c01d-teaser__grid-wrapper">
           <div class="dvag-m-c01d-teaser__content-wrapper">
-            <h4 class="dvag-m-c01d-teaser__subheadline">${args.subheadlineRight}</h4>
+            <h4 class="dvag-m-c01d-teaser__subheadline">${args.name}</h4>
             <div class="dvag-m-c01d-teaser__headline">Welchen Lebenstraum verfolgen Sie?</div>
             <div class="dvag-m-c01d-teaser__text">
               <p>
@@ -48,15 +55,20 @@ const teaserTemplateOverview = (args: TeaserTemplateArgs) => {
               </p>
             </div>
             <div class="dvag-m-c01d-teaser__button-wrapper">
+              <!-- linkType: vbcontactoverlay -->
+
               <dvag-e-c26-vb-contact-overlay-toggle
                 id="contact_genericLink"
                 class="dvag-e-c26-vb-contact-overlay-toggle dva-e-button dva-e-button--rebrush dva-e-button--small dva-state-active"
                 deeplinking-target="kontakt"
                 tabindex="0"
-                no-cookie-hide="no-cookie-hide">
+                no-cookie-hide="no-cookie-hide"
+              >
                 <div class="dva-e-button__background"></div>
                 <span class="dva-e-button__label">Jetzt Termin vereinbaren</span>
               </dvag-e-c26-vb-contact-overlay-toggle>
+
+              <!-- linkType: internalCrossContext -->
             </div>
           </div>
         </div>
@@ -64,16 +76,10 @@ const teaserTemplateOverview = (args: TeaserTemplateArgs) => {
     </div>
   `;
 };
-
-export default function renderTeaser(block: HTMLElement) {
-  const subheadlineRightElement = block.querySelector(".dvag-m-c01d-teaser__subheadline");
-  const subheadlineRight = subheadlineRightElement ? subheadlineRightElement.textContent : null;
+export default function (block: HTMLElement) {
+  const name = block.children[0].textContent;
+ 
   
-  if (!subheadlineRight) {
-    console.error("Subheadline not found");
-    return;
-  }
-
   cleanUpBlock(block);
-  render(teaserTemplateOverview({ subheadlineRight }), block);
+  render(teaserTemplateOverview(name), block);
 }
--- types/src/blocks/c01d-teaser/c01d-teaser.d.ts
+++ types/src/blocks/c01d-teaser/c01d-teaser.d.ts
@@ -1,2 +1,2 @@
 import "./c01d-teaser.scss";
-export default function renderTeaser(block: HTMLElement): void;
+export default function (block: HTMLElement): void;
