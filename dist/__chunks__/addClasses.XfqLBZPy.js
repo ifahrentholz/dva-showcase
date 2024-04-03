85bb51b1b6be7c461689686fea8bae629a991dbc
christian.schmidt <christian.schmidt@diva-e.com>
2024-04-03 16:54:59 +0200

    WIP : #27 optimize performance - build

---
+++ dist/__chunks__/addClasses.XfqLBZPy.js
@@ -0,0 +1,2 @@
+function t(a){return a.toLowerCase().replace(/[^0-9a-z]/gi,"-").replace(/-+/g,"-").replace(/^-|-$/g,"")}const r=(a,s)=>{s.split(",").forEach(e=>{a.classList.add(t(e.trim()))})};export{r as a,t};
+//# sourceMappingURL=addClasses.XfqLBZPy.js.map
---
+++ dist/__chunks__/dva-icon-cookie.CQE0T6cb.js
@@ -0,0 +1,2 @@
+const o="/dva-icon-cookie/dva-icon-cookie.svg";export{o as default};
+//# sourceMappingURL=dva-icon-cookie.CQE0T6cb.js.map
