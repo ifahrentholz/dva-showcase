dfc4f99a9f6c90b7c5879f4a3056ca16326ad19c
christian.schmidt <christian.schmidt@diva-e.com>
2024-04-03 09:27:41 +0200

    WIP : #27 bar component section - Article stage uses picture instead of lazy image to get loaded eager - Main service searched for img in LCP blocks and set them loaded eager

--- src/blocks/article-stage/article-stage.ts
+++ src/blocks/article-stage/article-stage.ts
@@ -1,10 +1,11 @@
-import { ifDefined } from "lit-html/directives/if-defined.js";
 import { html, render } from "lit";
 import "./article-stage.scss";
 import "../../components/dva-e-lazy-image/dva-e-lazy-image";
+import { cleanUpBlock } from "Utils/cleanUpBlock.ts";
+import { addClasses } from "Utils/addClasses.ts";
 
-const template = (imageSrc: string | undefined) => {
-  return html`<div class="dva-m-article-stage">
+const template = (picture: HTMLPictureElement | undefined) => {
+  return html` <div class="dva-m-article-stage">
     <div class="dva-m-article-stage__background">
       <div class="dva-m-skewed-box__content"></div>
       <div class="dva-h-skew--out"></div>
@@ -12,13 +13,7 @@ const template = (imageSrc: string | undefined) => {
     <div class="dva-grid">
       <div class="dva-grid-row">
         <div class="dva-grid-col-1-12 dva-grid-col-4-8 dva-grid-offset-4-2">
-          <div class="dva-m-article-stage__image">
-            <dva-e-lazy-image
-              class="dva-e-lazy-image dva-js-lazy-image"
-              src="${ifDefined(imageSrc)}"
-              aspect-ratio="2:1"
-            ></dva-e-lazy-image>
-          </div>
+          <div class="dva-m-article-stage__image">${picture}</div>
         </div>
       </div>
     </div>
@@ -26,9 +21,11 @@ const template = (imageSrc: string | undefined) => {
 };
 
 export default function (block: HTMLElement) {
-  const image: HTMLImageElement | null = block.querySelector("picture > img");
-  const src = image?.src;
-  block.innerHTML = "";
-  block.style.removeProperty("display");
-  render(template(src), block);
+  const picture = block.querySelector("picture") ?? undefined;
+  if (picture) {
+    addClasses(picture, "attachment-wide, size-wide");
+  }
+
+  cleanUpBlock(block);
+  render(template(picture), block);
 }
