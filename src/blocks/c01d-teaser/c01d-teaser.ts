a09ca83c9c4acc17575410aadd4f543f61c62009
izabela.horodenska <izabla.horodenska@diva-e.com>
2024-04-03 09:24:27 +0200

    added 2 variants-left,right side(adjustment table in GoogleDocs)

--- src/blocks/c01d-teaser/c01d-teaser.ts
+++ src/blocks/c01d-teaser/c01d-teaser.ts
@@ -1,26 +1,28 @@
+import { cleanUpBlock } from "Utils/cleanUpBlock";
 import { html, render } from "lit";
 import "./c01d-teaser.scss";
-import { cleanUpBlock } from "Utils/cleanUpBlock";
 
-interface TeaserSharingTemplateArgs {
-  name?: string | null;
-  url?: string | null;
+type variantName = "left" | "right";
+
+interface TeaserTemplateArgs {
+  subheadline?: string | null;
+  headline?: string | null;
+  textFirstPart?: string | null;
+  textSecondPart?: string | null;
+  btnText?: string | null;
+  variant?: variantName;
 }
 
-const teaserTemplateOverview = (args: TeaserSharingTemplateArgs[]) => {
+const teaserTemplateOverview = (args: TeaserTemplateArgs) => {
   return html`
     <div
-      class="c01d-teaser teaser dvag-h-background--gradient
-dvag-h-margin-bottom--none
-dvag-h-text--right
-dvag-h-headline--bold"
+      class="c01d-teaser teaser dvag-h-background--gradient dvag-h-margin-bottom--none dvag-h-headline--bold--${args.variant ||
+      "left"}"
     >
-      <div class="dvag-m-c01d-teaser  " style="--gradient-color:#CCDDE4">
+      <div class="dvag-m-c01d-teaser" style="--gradient-color:#CCDDE4">
         <div class="dvag-m-c01d-teaser__image-wrapper">
-         
-
           <dva-e-lazy-image
-            class="dva-e-lazy-image dva-js-lazy-image dva-h-preload dvag-m-c01d-teaser__image dva-h-preload--21-9 dvag-m-c01d-teaser__image--mobile"
+            class="dva-e-lazy-image dva-js-lazy-image dva-h-preload dvag-m-c01d-teaser__image dva-h-preload--21-9 dvag-m-c01d-teaser__image--mobile dva-state-initialized dva-state-invp dva-state-loaded"
             src="/content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original./teaser-ki-klopp-21_9-mobil.jpg"
             alt="teaser-ki-klopp-21_9-mobil.jpg"
             srcset="/content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.480.206.file/teaser-ki-klopp-21_9-mobil.jpg 480w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.760.326.file/teaser-ki-klopp-21_9-mobil.jpg 760w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.1024.439.file/teaser-ki-klopp-21_9-mobil.jpg 1024w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg./cq5dam.web.1280.1280.jpeg 1280w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original.image_file.1440.617.file/teaser-ki-klopp-21_9-mobil.jpg 1440w, /content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9-mobil.jpg/_jcr_content/renditions/original./teaser-ki-klopp-21_9-mobil.jpg 1460w"
@@ -28,7 +30,6 @@ dvag-h-headline--bold"
             aspect-ratio="21:9"
           >
           </dva-e-lazy-image>
-
           <dva-e-lazy-image
             class="dva-e-lazy-image dva-js-lazy-image dva-h-preload dvag-m-c01d-teaser__image dva-h-preload--21-9 dvag-m-c01d-teaser__image--desktop dva-state-initialized dva-state-invp dva-state-loaded"
             src="/content/dam/projects/dvag/bilder/klopp/teaser-ki-klopp-21_9.jpg/_jcr_content/renditions/original./teaser-ki-klopp-21_9.jpg"
@@ -41,22 +42,13 @@ dvag-h-headline--bold"
         </div>
         <div class="dvag-m-c01d-teaser__grid-wrapper">
           <div class="dvag-m-c01d-teaser__content-wrapper">
-            <h4 class="dvag-m-c01d-teaser__subheadline">${args.name}</h4>
-            <div class="dvag-m-c01d-teaser__headline">Welchen Lebenstraum verfolgen Sie?</div>
+            <h4 class="dvag-m-c01d-teaser__subheadline">${args.subheadline}</h4>
+            <div class="dvag-m-c01d-teaser__headline">${args.headline}</div>
             <div class="dvag-m-c01d-teaser__text">
-              <p>
-                Manchmal ist es nur eine einzige Entscheidung, die zu einem ganz anderen Leben führt.&nbsp;Gerade in den
-                entscheidenden Momenten ist es wichtig, jemanden an seiner Seite zu haben, dem man vertrauen kann. Der
-                einen coacht. Und der einem hilft, das Beste aus seinem Leben rauszuholen.&nbsp;
-              </p>
-              <p>
-                Wie auch immer Sie Ihr Leben leben wollen: Ich helfe Ihnen, die richtigen finanziellen Entscheidungen
-                zu&nbsp;treffen.
-              </p>
+              <p>${args.textFirstPart}</p>
+              <p>${args.textSecondPart}</p>
             </div>
             <div class="dvag-m-c01d-teaser__button-wrapper">
-              <!-- linkType: vbcontactoverlay -->
-
               <dvag-e-c26-vb-contact-overlay-toggle
                 id="contact_genericLink"
                 class="dvag-e-c26-vb-contact-overlay-toggle dva-e-button dva-e-button--rebrush dva-e-button--small dva-state-active"
@@ -65,10 +57,8 @@ dvag-h-headline--bold"
                 no-cookie-hide="no-cookie-hide"
               >
                 <div class="dva-e-button__background"></div>
-                <span class="dva-e-button__label">Jetzt Termin vereinbaren</span>
+                <span class="dva-e-button__label">${args.btnText}</span>
               </dvag-e-c26-vb-contact-overlay-toggle>
-
-              <!-- linkType: internalCrossContext -->
             </div>
           </div>
         </div>
@@ -76,10 +66,42 @@ dvag-h-headline--bold"
     </div>
   `;
 };
-export default function (block: HTMLElement) {
-  const name = block.children[0].textContent;
- 
-  
+
+export default function renderTeaser(block: HTMLElement) {
+  let subheadline: string | null = null;
+  let headline: string | null = null;
+  let textFirstPart: string | null = null;
+  let textSecondPart: string | null = null;
+  let btnText: string | null = null;
+
+  const variant = [...block.classList].filter(
+    classValue => classValue !== "block" && classValue !== "c01d-teaser",
+  )[0] as TeaserTemplateArgs["variant"];
+
+  if (variant === "right") {
+    subheadline = block.querySelector(".c01d-teaser > div:nth-child(1) > div:nth-child(1)")?.textContent ?? null;
+    headline = block.querySelector(".c01d-teaser > div:nth-child(2) > div:nth-child(1)")?.textContent ?? null;
+    textFirstPart = block.querySelector(".c01d-teaser > div:nth-child(3) > div:nth-child(1)")?.textContent ?? null;
+    textSecondPart = block.querySelector(".c01d-teaser > div:nth-child(4) > div:nth-child(1)")?.textContent ?? null;
+    btnText = block.querySelector(".c01d-teaser > div:nth-child(5) > div:nth-child(1)")?.textContent ?? null;
+  } else if (variant === "left") {
+    subheadline = block.querySelector(".c01d-teaser > div:nth-child(1) > div:nth-child(2)")?.textContent ?? null;
+    headline = block.querySelector(".c01d-teaser > div:nth-child(2) > div:nth-child(2)")?.textContent ?? null;
+    textFirstPart = "";
+    textSecondPart = "";
+    btnText = block.querySelector(".c01d-teaser > div:nth-child(5) > div:nth-child(2)")?.textContent ?? null;
+  }
+
   cleanUpBlock(block);
-  render(teaserTemplateOverview(name), block);
+
+  const args: TeaserTemplateArgs = {
+    subheadline,
+    headline,
+    textFirstPart,
+    textSecondPart,
+    btnText,
+    variant,
+  };
+
+  render(teaserTemplateOverview(args), block);
 }
